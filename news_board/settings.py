870ce7cfc6d8d22b62cdbc186d2163fbf3d6e569
AndriiNartov <nartov1912@gmail.com>
2021-11-05 07:21:22 +0100

    test host added to allowed

--- news_board/settings.py
+++ news_board/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["news-board-application.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["news-board-application.herokuapp.com", "127.0.0.1", "stepic-vacancies-week2.herokuapp.com"]
 
 
 # Application definition
